64b2abb1aee625deb229ba4e0fb857d8d10d408c
brianjrosenthal <brian.rosenthal@gmail.com>
2026-01-01 14:10:32 -0500

    move homepage to its own html  file.

--- theme/ResponsiveCE/homepage.php
+++ theme/ResponsiveCE/homepage.php
@@ -1,7 +1,10 @@
 <?php if (!defined('IN_GS')) { die('you cannot load this page directly.'); } ?>
 <?php include('header.inc.php'); ?>
 
-
-				<?php get_page_content(); ?>
+				<?php 
+				// Homepage content is now stored in an external HTML file for easier editing
+				// No need to deal with XML escaping anymore!
+				include('homepage-content.html'); 
+				?>
 
 <?php include('footer.inc.php'); ?>
